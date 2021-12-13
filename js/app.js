e1b1b3651c48086cb4180dcd864e4a828150f101
Erik Daniel <erikrame69@gmail.com>
2021-12-13 17:32:43 -0600

    Aprendiendo a usar la consola

--- js/app.js
+++ js/app.js
@@ -1,2 +1,5 @@
 alert('Hola desde un archivo externo'); /*De este lado tambien tenemos alertas, el 
 orden del JS es importante al momento de declararlo en  el HTML*/
+
+console.log("Hola consola"); /*Podemos enviar mensajes directamente a la consola 
+para eso usaremos || console.log("Hola consola"); ||  */
