d7a3c56eb18a230d71812de2df3f06f41fabb968
Erik Daniel <erikrame69@gmail.com>
2021-12-13 18:08:48 -0600

    Escribiendo comentarios en JavaScript

--- js/app.js
+++ js/app.js
@@ -3,3 +3,7 @@ orden del JS es importante al momento de declararlo en  el HTML*/
 
 console.log("Hola consola"); /*Podemos enviar mensajes directamente a la consola 
 para eso usaremos || console.log("Hola consola"); ||  */
+
+//Hay dos formas de hacer comentarios con doble diagonal (//) o comentario de bloque (/* Informacion a ignorar*/)
+/*Así podemos comentar el código para tener una mejor perpectiva de lo que hicimos
+Esto es un comentario en bloque */
