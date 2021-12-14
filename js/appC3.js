49bf3999d64e0cd01f1a66fdcb96fa9c7751f896
Erik Daniel <erikrame69@gmail.com>
2021-12-13 19:35:30 -0600

    Trabajar con números en JavaScript

---
+++ js/appC3.js
@@ -0,0 +1,16 @@
+"use strict"
+
+/******************* || TIPO DE DATO NUMERICO || *******************/
+var edad = 35;
+var edad1 = 40.5;
+var edad2 = -35;
+//Podemos usar numeros enteros, flotante, positivos y negativos.
+
+var cantidad = "100"; //Los numeros no llevan comillas porque este tipo de datos se interpretan como un string.
+console.log(cantidad);
+
+var nuevaCantidad = Number(cantidad);
+
+parseInt(); //Lo usamos para convertir a numero entero.
+parseFloat(); //Lo usamos para convertir un numero a flotante.
+ 
