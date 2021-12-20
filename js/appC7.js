de71c258d6fe509bbca4e97494873a057ef91e21
Erik Daniel <erikrame69@gmail.com>
2021-12-20 16:25:05 -0600

    Parametros de una funcion

--- js/appC7.js
+++ js/appC7.js
@@ -4,7 +4,7 @@ console.log("\n************* C6 - USO DE CONDICIONES O DECISIONES EN JAVASCRIPT 
 
 
 /******************* || ESTRUCTURA BASICA DE UNA FUNCION || *******************/
-console.log("\nESTRUCTURA BÁSICA DE UNA FUNCIÓN");
+console.log("\n|| ESTRUCTURA BÁSICA DE UNA FUNCIÓN");
 
 /*Estructura de la funcion 
 
@@ -37,3 +37,45 @@ function        saludar                         ()                  {
 
 saludar();//Aqui llamamos a la funcion para que esta se pueda ejecutar.
 //Tambien podemos invocar funciones desde la consola
+
+
+
+/******************* || PARAMETROS DE UNA FUNCION || *******************/
+console.log("\n|| PARÁMETROS DE UNA FUNCIÓN");
+
+/*En este caso le pasaremos los parametros a la funcion saludar
+Como vemos en los parametros colocamos nombre y edad, estos son las variables
+con las que nuestra funcion va a trabajar*/
+
+function saludando(nombre, edad) {
+
+    console.log("Hola: " + nombre); /*Aqui concatenamos una cadena de texto 
+    con los valores asignado en la linea 65 */
+
+    console.log("Edad: " + edad);/*Aqui concatenamos una cadena de texto 
+    con los valores asignado en la linea 65 */
+
+    var resultado = nombre + " tiene " + edad + " años";/*De este lado creamos
+    una variable llamada resultado donde concatenamos lo parametros con una 
+    cadena de texto*/
+
+    return resultado; //Retornamos el valor de la variable resultado
+}
+
+var mensaje = saludando("@ErikDanielRM", 22); /*Nosotros estamos llamando a la 
+funcion desde aqui y le estamos asigando algunos valores a a los parametros 
+declarado en la linea 50, en pocas palabras le estamos parsando el nombre y la 
+edad para que la funcion pueda trabajar con estos valores
+
+Como tambien podemos observar estamos asigando el calor de la funcion saludando 
+a una variable llamada mensaje, de esta forma podemos llamar asigar todo lo que
+se encuentra dentro de la funcion a la variable y podemos llamar a "mensaje" desde
+cualquier parte del codigo, y esta nos mostrara todo lo que ha ejecutado la
+funcion.*/
+
+console.log(mensaje);/*Como "saludando" se a asignado a "mensaje" al llamar a 
+mensaje nos mostrara lo que la funcion contiene. */
+
+/*IMPRIME:      Hola: @ErikDanielRM
+                Edad: 22
+                @ErikDanielRM tiene 22 años     */
