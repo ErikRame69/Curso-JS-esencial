266039ec4d6909cc9711caa0d79801c920621590
Erik Daniel <erikrame69@gmail.com>
2021-12-30 15:58:34 -0600

    Busqueda de texto por indices y expresiones regulares

--- js/appC11.js
+++ js/appC11.js
@@ -60,3 +60,41 @@ incluidos los espacion */
 
 console.log("La cadena de texto tiene: " + mensaje.length + " letras");
 //Imprime: 5
+
+
+
+/******************* || BUSQUEDA DE TEXTO POR INDICES Y POR EXPRESIONES REGULARES || *******************/
+console.log("\n|| BUSQUEDA DE TEXTO POR INDICES Y POR EXPRESIONES REGULARES");
+
+var mensaje2 = "Estoy aprendiendo JavaScript y estoy aprendiendo mucho";
+
+var resultado;
+
+//indexOf
+console.log("** indexOf ** ");
+resultado = mensaje2.indexOf("aprendiendo");
+console.log(resultado);
+//Nos muestra la primer posicion de la primer incidencia en la cadena de texto
+
+//lastIndexOf
+console.log("** lastIndexOf ** ");
+resultado = mensaje2.lastIndexOf("aprendiendo");
+console.log(resultado);
+//Nos muestra la primera posicion de la ultima incidencia hallada en la cadena de texto
+
+//search
+console.log("** search ** ");
+resultado = mensaje2.search("aprendiendo");
+console.log(resultado);
+//Es muy similar al indexOf asi que podemos usar indexOf o search
+
+//serch | Expresion regular
+console.log("** serch | Expresión regular ** ");
+resultado = mensaje2.search(/ja/i);/*Esta expresion se coloca dentro de dos barras
+// dentro de las barras es donde indicamos que queremos que busque:
+/Aqui va la expresion a buscar/
+y por fuera podemos poner diferentes parametros, como por ejemplo que sea incase 
+sensitive:
+" /Aqui va la expresion a buscar/i " la i nos indica que la busqueda sera insencible
+a mayusculas o minusculas*/
+console.log(resultado);
