21cda10e10f1a3939884e2c6d216b5a465988cb3
Erik Daniel <erikrame69@gmail.com>
2021-12-30 23:53:49 -0600

    Buscar en un arreglo de JavaScript

--- js/appC13.js
+++ js/appC13.js
@@ -24,3 +24,27 @@ platillos.forEach(platillo => console.log(platillo));
 
 console.log("----------------Mostrando el índice----------------");
 platillos.forEach((platillo, i) => console.log(i, platillo));
+
+
+
+/******************* || BUSCAR EN UN ARREGLO DE JAVASCRIPT || *******************/
+console.log("\n|| BUSCAR EN UN ARREGLO DE JAVASCRIPT");
+
+var platillos = ["Tacos", "Langosta", "Lasaña", "Salmón", "Filete Kobe"];
+
+var plaElegido = platillos.find( platillo => platillo == "Tacos")
+console.log(plaElegido);
+//Muestra: Tacos
+
+var menu = [
+    {nombre:"Tacos", precio:20, pais:"México"},
+    {nombre:"Langosta", precio:1500, pais:"Alemania"},
+    {nombre:"Lasaña", precio:425, pais:"Italia"},
+    {nombre:"Salmón", precio:800, pais:"Rusia"},
+    {nombre:"Filete Kobe", precio:4500, pais:"Japón"}
+];
+var plaElegido = menu.find(platillo => platillo.nombre == "Lasaña")
+console.log(plaElegido);
+//Nos muestra todo el objeto
+/*Esta formas de buscar dentro de un arreglo son muy utlies para encontrar un 
+determinado dato dentro de un arreglo */
