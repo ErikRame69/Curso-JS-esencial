e9ff553f0d4b23a44e342248805bdf85ee8d104e
Erik Daniel <erikrame69@gmail.com>
2022-01-01 20:55:16 -0600

    Trabajar con clases en la programacion orientada a objetos

---
+++ js/appC16.js
@@ -0,0 +1,16 @@
+"use strict" //Activamos el moso estricto para evitar conflictos futuros
+
+console.log("\n************* C16 - PROGRAMACIÓN ORIENTADA A OBJETOS ************");-
+
+/******************* || TRABAJAR CON CLASES EN LA PROGRAMACION ORIENTADA A OBJETOS || *******************/
+console.log("\n|| TRABAJAR CON CLASES EN LA PROGRAMACIÓN ORIENTADA A OBJETOS");
+
+class Pantalla { /*Aqui nosotros estamos creando la clase pantalla */
+    constructor(){ /*Todas las clases que creemos necesitan un constructor*/
+
+    }
+}
+
+const tvSala = new Pantalla(); //Esto es la instancia de una clase
+
+const tvHabitacion = new Pantalla();//Esto es la instancia de una clase
