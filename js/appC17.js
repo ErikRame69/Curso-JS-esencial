3242d451316278e4e6e55c16cf0ce99975f4fe7c
Erik Daniel <erikrame69@gmail.com>
2022-01-01 22:58:44 -0600

    Generación de errores personalizados con JavaScript

--- js/appC17.js
+++ js/appC17.js
@@ -24,3 +24,25 @@ try{
 
 }/*De esta forma podemos saber que tipo de error tenemos e incluso podemos observar
 un mensaje que nos describe el error*/
+
+/******************* || GENERACIÓN DE ERRORES PERSONALIZADOS EN JAVASCRIPT || *******************/
+console.log("\n|| GENERACIÓN DE ERRORES PERSONALIZADOS EN JAVASCRIPT");
+
+let valor1 = 100;
+let valor2 = 200;
+
+try {
+    if (valor1 > valor2){
+        console.log(`Mensaje de validación: ${valor1} si es mayor que ${valor2}`);
+    }else {
+        throw new Error(`${valor1} no es mayor que ${valor2} :(`);
+        /*El método throw es como si detonara algo, en este caso detonaremos un
+        error
+        El detonar errores es muy util porque asi podemos advertir al usuario que 
+        algun datos, parametro o algo esta mal y podemos decir que es.*/
+    }
+}catch(error){
+    console.log(error);
+    console.log(error.message);
+    console.log(error.name);
+}
